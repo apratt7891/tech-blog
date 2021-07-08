70da92d05636bee6318d45a81df71b2d31dd042a
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-07-07 21:07:44 -0600

    change api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
+  
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
