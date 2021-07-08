0050215d53f04f3f7760d568e17bb8415b4df34a
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-07-07 20:49:07 -0600

    uncomment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const serverRoutes = require('./controllers');
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret',
